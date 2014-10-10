3971a20841f4f11bd9e87a8506479d7478c4eedc
ktakuya <a0124973@WS03.(none)>
2014-10-10 14:40:25 +0900

    Add FastDocumentSearcher

---
+++ FastDocumentSearcher/FastDocumentSearcher/DocumentSearcher.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastDocumentSearcher
+{
+    /// <summary>
+    /// 文書群に対する高速な検索クラス
+    /// </summary>
+    class DocumentSearcher
+    {
+        /// <summary>
+        /// コンストラクタ 
+        /// </summary>
+        /// <param name="docs">検索対象であるDocumentクラスからなる文書のリスト</param>
+        public DocumentSearcher(List<Document> docs)
+        {
+
+        }
+
+        /// <summary>
+        /// 指定されたクエリによる検索結果を返すメソッド
+        /// </summary>
+        /// <param name="query">検索クエリ</param>
+        /// <returns>関連性が高い順にランキングされたDocumentクラスのリスト</returns>
+        public List<Document> Search(string query)
+        {
+            List<Document> documentList = new List<Document>();
+            return documentList;
+        }
+
+    }
+
+
+    /// <summary>
+    /// タイトルと本文を持つドキュメントクラス
+    /// </summary>
+    class Document
+    {
+
+        private string _title;
+        private string _body;
+
+        /// <summary>
+        /// プロパティ
+        /// </summary>
+        public string Title
+        {
+            get { return _title; }
+        }
+
+        /// <summary>
+        /// プロパティ
+        /// </summary>
+        public string Body
+        {
+            get { return _body; }
+        }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="title">文書のタイトル</param>
+        /// <param name="body">文書の本文</param>
+        public Document(string title, string body)
+        {
+            _title = title;
+            _body = body;
+        }
+    }
+}
---
+++ FastDocumentSearcher/FastDocumentSearcher/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastDocumentSearcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Document> docs = new List<Document>();
+            docs.Add(new Document("タイトル", "本文")); // 文書群作成
+            DocumentSearcher ds = new DocumentSearcher(docs);
+            string query = "京都";
+            List<Document> rankedDoc = ds.Search(query);
+        }
+    }
+}
