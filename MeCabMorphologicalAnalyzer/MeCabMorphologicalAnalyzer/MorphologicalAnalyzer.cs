52b890b815cfa0441cce2ce9a8bda52d4b48773d
ktakuya <a0124973@WS03.(none)>
2014-10-10 16:46:53 +0900

    Complete MorphologicalAnalyzer

--- MeCabMorphologicalAnalyzer/MeCabMorphologicalAnalyzer/MorphologicalAnalyzer.cs
+++ MeCabMorphologicalAnalyzer/MeCabMorphologicalAnalyzer/MorphologicalAnalyzer.cs
@@ -9,12 +9,22 @@ namespace MeCabMorphologicalAnalyzer
 {
     class MeCabMorphologicalAnalyzer
     {
+       /// <summary>
+       /// コンストラクタ
+       /// </summary>
         public MeCabMorphologicalAnalyzer() { }
 
+        /// <summary>
+        /// 解析メソッド
+        /// </summary>
+        /// <param name="str">解析する文字列</param>
+        /// <returns>解析された結果をまとめたリスト</returns>
         public List<Morpheme> Analyse(string str)
         {
+            // 結果を格納するリストを生成
             List<Morpheme> morphemeList = new List<Morpheme>();
 
+            // MeCab
             MeCabTagger mct = MeCabTagger.Create();
             MeCabNode node = mct.ParseToNode(str);
 
@@ -22,11 +32,21 @@ namespace MeCabMorphologicalAnalyzer
             {
                 if (node.CharType > 0)
                 {
-                    Console.WriteLine(node.Surface + " - " + node.Feature);
+                    morphemeList.Add(new Morpheme(node.Surface, GetPos(node.Feature)));
                 }
                 node = node.Next;
             }
             return morphemeList;
+        }
+
+        /// <summary>
+        /// 品詞を返すメソッド
+        /// </summary>
+        /// <param name="feature">,で区切られた特徴を表す文字列</param>
+        /// <returns>品詞</returns>
+        private string GetPos(string feature)
+        {
+            return feature.Split(',')[0];
         }
     }
 }
