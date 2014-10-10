0728059583a535ce77565fe5130e8b6701e96ae1
ktakuya <a0124973@WS03.(none)>
2014-10-10 16:39:02 +0900

    Create MeCabMorphologicalAnalyzer project

---
+++ MeCabMorphologicalAnalyzer/MeCabMorphologicalAnalyzer/MorphologicalAnalyzer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NMeCab;
+
+namespace MeCabMorphologicalAnalyzer
+{
+    class MeCabMorphologicalAnalyzer
+    {
+        public MeCabMorphologicalAnalyzer() { }
+
+        public List<Morpheme> Analyse(string str)
+        {
+            List<Morpheme> morphemeList = new List<Morpheme>();
+
+            MeCabTagger mct = MeCabTagger.Create();
+            MeCabNode node = mct.ParseToNode(str);
+
+            while (node != null)
+            {
+                if (node.CharType > 0)
+                {
+                    Console.WriteLine(node.Surface + " - " + node.Feature);
+                }
+                node = node.Next;
+            }
+            return morphemeList;
+        }
+    }
+}
---
+++ MeCabMorphologicalAnalyzer/MeCabMorphologicalAnalyzer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MeCabMorphologicalAnalyzer;
+
+namespace MeCabMorphologicalAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "今日はいい天気です";
+            MeCabMorphologicalAnalyzer analyser = new MeCabMorphologicalAnalyzer();
+            List<Morpheme> result = analyser.Analyse(str);
+        }
+    }
+}
