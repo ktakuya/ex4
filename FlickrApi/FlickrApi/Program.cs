f26c657556ce763dcd14bec861f595877d85bc74
ktakuya <a0124973@WS03.(none)>
2014-10-10 14:16:50 +0900

    Add Test Case

--- FlickrApi/FlickrApi/Program.cs
+++ FlickrApi/FlickrApi/Program.cs
@@ -11,6 +11,18 @@ namespace FlickrApi
         static void Main(string[] args)
         {
             string key = "2b4f6b0fb919de79b3e9595a909d4845";
+            string tags = "東京";
+            FlickrApi flickr = new FlickrApi(key);
+            List<FlickrImage> result = flickr.Search(tags);
+            /*
+            foreach (FlickrImage fl in result)
+            {
+                Console.WriteLine("------------------------");
+                Console.WriteLine(fl.ImageUrl);
+                Console.WriteLine(fl.OwnerName);
+                Console.WriteLine(fl.Title);
+            }
+            */
         }
     }
 }
