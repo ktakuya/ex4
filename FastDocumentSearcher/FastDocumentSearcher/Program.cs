41479e15e37b0abba3aa65b0d136bacd34c093cc
ktakuya <taakuu19@gmail.com>
2014-10-16 14:03:46 +0900

    Add test cases

--- FastDocumentSearcher/FastDocumentSearcher/Program.cs
+++ FastDocumentSearcher/FastDocumentSearcher/Program.cs
@@ -11,7 +11,8 @@ namespace FastDocumentSearcher
         static void Main(string[] args)
         {
             List<Document> docs = new List<Document>();
-            docs.Add(new Document("タイトル", "本文")); // 文書群作成
+            docs.Add(new Document("タイトル", "ソート済みのハッシュテーブルの利用")); // 文書群作成
+            docs.Add(new Document("title2", "すべてのソートを利用"));
             DocumentSearcher ds = new DocumentSearcher(docs);
             string query = "京都";
             List<Document> rankedDoc = ds.Search(query);
