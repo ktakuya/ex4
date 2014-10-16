c7b3f793e8e96424c507859aa46014c0b86ec4e5
ktakuya <taakuu19@gmail.com>
2014-10-16 14:00:07 +0900

    Add MeCab files

---
+++ FastDocumentSearcher/FastDocumentSearcher/MeCabMorphologicalAnalyzer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NMeCab;
+
+namespace MeCabMorphologicalAnalyzer
+{
+    class MeCabMorphologicalAnalyzer
+    {
+       /// <summary>
+       /// コンストラクタ
+       /// </summary>
+        public MeCabMorphologicalAnalyzer() { }
+
+        /// <summary>
+        /// 解析メソッド
+        /// </summary>
+        /// <param name="str">解析する文字列</param>
+        /// <returns>解析された結果をまとめたリスト</returns>
+        public List<Morpheme> Analyse(string str)
+        {
+            // 結果を格納するリストを生成
+            List<Morpheme> morphemeList = new List<Morpheme>();
+
+            // MeCab
+            MeCabTagger mct = MeCabTagger.Create();
+            MeCabNode node = mct.ParseToNode(str);
+
+            while (node != null)
+            {
+                if (node.CharType > 0)
+                {
+                    morphemeList.Add(new Morpheme(node.Surface, GetPos(node.Feature)));
+                }
+                node = node.Next;
+            }
+            return morphemeList;
+        }
+
+        /// <summary>
+        /// 品詞を返すメソッド
+        /// </summary>
+        /// <param name="feature">,で区切られた特徴を表す文字列</param>
+        /// <returns>品詞</returns>
+        private string GetPos(string feature)
+        {
+            return feature.Split(',')[0];
+        }
+    }
+}
---
+++ FastDocumentSearcher/FastDocumentSearcher/Morpheme.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeCabMorphologicalAnalyzer
+{
+    /// <summary>
+    /// 解析された１つワードを表すクラス
+    /// </summary>
+    class Morpheme
+    {
+        private string _surface;
+        private string _pos;
+
+        /// <summary>
+        /// プロパティ
+        /// </summary>
+        public string Surface
+        {
+            get { return _surface; }
+        }
+
+        /// <summary>
+        /// プロパティ
+        /// </summary>
+        public string Pos
+        {
+            get { return _pos; }
+        }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="surface">解析された文字</param>
+        /// <param name="pos">その品詞</param>
+        public Morpheme(string surface, string pos)
+        {
+            this._surface = surface;
+            this._pos = pos;
+        }
+
+        /// <summary>
+        /// 文字と品詞をあわせた文字列を返す
+        /// </summary>
+        /// <returns>整形された文字列</returns>
+        public override string ToString()
+        {
+            return string.Format("{0} ({1})", this._surface, this._pos);
+        }
+    }
+}
