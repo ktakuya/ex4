0070e5274063b12c85a16e422257a52e11933dbd
ktakuya <taakuu19@gmail.com>
2014-10-23 15:08:06 +0900

    Add NaiveVayes project

---
+++ NaiveBayes/NaiveBayes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaiveBayes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
